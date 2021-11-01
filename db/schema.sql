af8ebe8eab6febcb48f2bb7123b0bfe015bff1ad
Shayne Lemons <lemonsshayne6@gmail.com>
2021-11-01 13:03:52 -0700

    add get, post and delete routes for departments

--- db/schema.sql
+++ db/schema.sql
@@ -2,20 +2,20 @@ DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
     description TEXT
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department (id)department_id,
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router =express.router();
+const router = express.router();
 const db = require('../../db/connection');
 
 // get all departments 
@@ -11,10 +11,62 @@ router.get('/departments', (req, res) => {
             res.status(500).json({ error: err.message });
             return;
         }
-        res.json({ 
+        res.json({
             message: 'query successful',
 
         });
     });
 });
 
+// add a department
+router.post('/department', ({ body }, res) => {
+    const errors = validateInput(
+        body,
+        'name',
+        'description'
+    );
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return
+    }
+
+    const sql = `INSERT INTO departments (name, description) VALUES (?,?)`;
+    const params = [
+        body.name,
+        body.description
+    ];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'query successful',
+            data: body
+        });
+    });
+});
+
+// delete a department
+router.delete('/department/:id', (req, res) => {
+    const sql = `DELETE FROM department WHERE id = ?`;
+
+    db.query(sql, req.params.id, (err, results) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Department not found'
+            });
+        } else {
+            res.json({
+                message: 'Department deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+module.exports = router;
