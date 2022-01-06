248dc6aa9f73462bd19d58ae2064a2e33db51a37
Shayne Lemons <lemonsshayne6@gmail.com>
2022-01-06 08:40:37 -0800

    finish seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,33 @@
-INSERT INTO departments
+use employees;
+
+INSERT INTO department
 (name)
-VALUES ('Sales');
-
-INSERT INTO ROLES
-(title, salary, department_id)
-VALUES ('Salesperson', 50000.00, (SELECT id FROM departments WHERE name = 'Sales'));
-
-INSERT INTO employees 
-(first_name, last_name, role_id)
 VALUES 
-('John', 'Doe', (SELECT id FROM roles WHERE title='Salesperson'));
+('Engineering'),
+('Finance'),
+('Legal'),
+('Sales');
+
+INSERT INTO role
+(title, salary, department_id)
+VALUES 
+('Lead Engineer', 125000, 1),
+('Software Engineer', 100000, 1),
+('Account Manager', 110000, 2),
+('Accountant', 90000, 2),
+('Legal Team Lead', 225000, 3),
+('Lawyer', 175000, 3),
+('Sales Lead', 100000, 4),
+('Salesperson', 50000.00, 4);
+
+INSERT INTO employees
+(first_name, last_name, role_id, manager_id,)
+VALUES 
+('Bruce', 'Wayne', 1, NULL),
+('Clark', 'Kent', 2, 1),
+('Barry', 'Allen', 3, NULL),
+('Wally', 'West', 4, 3),
+('Peter', 'Parker', 5, NULL),
+('Miles', 'Morales', 6, 5),
+('Diana', 'Prince', 7, NULL);
+('Jim', 'Gordon', 8, 7);
